4cd1be7e543a0c0666013ba4363008d65d189d5e
jeffery <dungeonsnd@gmail.com>
2014-12-09 06:36:41 +0800

    add multiver

---
+++ multiver/src/libevent/libsu_new_program.c
@@ -0,0 +1,9 @@
+
+#include "libsu_new_program.h"
+
+int libsu_new_recv_listenfds()
+{
+	int res =0;
+	return res;
+}
+
---
+++ multiver/src/libevent/libsu_new_program.h
@@ -0,0 +1,13 @@
+#ifndef _HEADER_FILE_LIBSU_NEW_PROGRAM_H_
+#define _HEADER_FILE_LIBSU_NEW_PROGRAM_H_
+
+
+int libsu_new_recv_listenfds();
+int libsu_new_start_monitor_listenfds();
+
+int libsu_new_recv_sessions();
+int libsu_new_process_sessions();
+int libsu_new_start_monitor_sessions();
+
+
+#endif // _HEADER_FILE_LIBSU_NEW_PROGRAM_H_
