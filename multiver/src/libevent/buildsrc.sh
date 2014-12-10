8f6b3a0c132df08131dd69e3e59a0cc87d1dd23a
jeffery <dungeonsnd@gmail.com>
2014-12-10 17:29:56 +0800

    add src

--- multiver/src/libevent/buildsrc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-gcc -Wall -g libsu_new_program.c libsu_old_program.c -fPIC -shared -o libsu.so   \
--I../../../../cppmaven/cpprepos/libevent-2.0.21-stable/include/   \
--L../../../../cppmaven/cpprepos/libevent-2.0.21-stable/lib/ -levent_core
